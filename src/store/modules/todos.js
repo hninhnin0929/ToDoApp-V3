8158409fd0cb5ee9265b20ff36931c94d9f1a99d
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-27 16:43:33 +0900

    add updateTodo action and mutation

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -45,13 +45,24 @@ const actions = {
         const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`);
         console.log(response.data);
         commit('setTodos', response.data);
+    },
+    async updateTodo({ commit }, updTodo){
+        const response = await axios.put(`https://jsonplaceholder.typicode.com/todos/${updTodo.id}`);
+
+        commit('updTodo', updTodo);
     }
 };
 
 const mutations = {
     setTodos: (state, todos) => (state.todos = todos),
     newTodo: (state, todo) => state.todos.unshift(todo),
-    removeTodo: (state, id) => (state.todos = state.todos.filter(todo => todo.id !== id))
+    removeTodo: (state, id) => (state.todos = state.todos.filter(todo => todo.id !== id)),
+    updateTodo: (state, updTodo) => {
+        const index = state.todos.findIndex(todo => todo.id === updTodo.id);
+        if(index !== -1){
+            state.todos.splice(index, 1, updTodo);
+        }
+    }
 };
 
 export default {
