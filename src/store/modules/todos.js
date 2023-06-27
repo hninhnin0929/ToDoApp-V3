9b664b0de4d976c76d6a543b8077e2789e18f747
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-27 13:07:31 +0900

    add addTodo action and mutation

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -14,24 +14,33 @@ const state = {
 };
 
 const getters = {
-    allTodos: (state)=> state.todos
+    allTodos: (state) => state.todos
 };
 
 const actions = {
-    async fetchTodos({ commit }){
+    async fetchTodos({ commit }) {
         const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
 
         commit('setTodos', response.data);
+    },
+    async addTodo({ commit }, title) {
+        const response = await axios.post('https://jsonplaceholder.typicode.com/todos', {
+            title, completed: false
+        });
+        console.log("addTodo = ", response.data);
+
+        commit('newTodo', response.data);
     }
 };
 
 const mutations = {
-    setTodos: (state, todos) => (state.todos = todos)
+    setTodos: (state, todos) => (state.todos = todos),
+    newTodo: (state, todo) => state.todos.unshift(todo)
 };
 
 export default {
-   state,
-   getters,
-   actions,
-   mutations 
+    state,
+    getters,
+    actions,
+    mutations
 }
